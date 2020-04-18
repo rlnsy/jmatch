7644595827e655ac39a909efcb0dfe704f7c7c7f
rlnsy <rowlindsay@gmail.com>
2020-04-18 12:49:31 -0700

    add relevant test cases

--- test/match.spec.ts
+++ test/match.spec.ts
@@ -81,4 +81,74 @@ describe("Match function", () => {
         }
     });
 
+    it("Should be able to match multiline strings by default", () => {
+        let ran = false;
+        match("my name is\n Rowan and I'm 21 \nyears old,22;", [
+            ["my name is\n ,1; and I'm ,2; \nyears old,22;",
+                (name, age) => {
+                    ran = true;
+                    expect(age).to.equal("21");
+                    expect(name).to.equal("Rowan");
+                }]
+        ]);
+        if (!ran) {
+            expect.fail("Did not run case code");
+        }
+    });
+
+    it("Should not match multiline strings if newline not in pattern", () => {
+        try {
+            match("my name is\n Rowan and I'm 21 \nyears old,22;", [
+                ["my name is ,1; and I'm ,2; years old,22;",
+                    () => {
+                        expect.fail("Ran case code")
+                    }]
+            ]);
+        } catch (err) {
+            expect(err).to.be.an.instanceOf(Error);
+        }
+    });
+
+    it("Should allow multiline strings at the end of grouped values", () => {
+        let ran = false;
+        match("my name is Rowan\n and I'm 21 years old,22;", [
+            ["my name is ,1; and I'm ,2; years old,22;",
+                (name) => {
+                    ran = true;
+                    expect(name).to.equal("Rowan\n");
+                }]
+        ]);
+        if (!ran) {
+            expect.fail("Did not run case code");
+        }
+    });
+
+    it("Should allow multiline strings in the middle of grouped values", () => {
+        let ran = false;
+        match("my name is Rowan\nLindsay and I'm 21 years old,22;", [
+            ["my name is ,1; and I'm ,2; years old,22;",
+                (name) => {
+                    ran = true;
+                    expect(name).to.equal("Rowan\nLindsay");
+                }]
+        ]);
+        if (!ran) {
+            expect.fail("Did not run case code");
+        }
+    });
+
+    it("Should allow escaped quotes to be matched", () => {
+        let ran = false;
+        match("my name is \"Rowan\" and I'm 21 years old,22;", [
+            ["my name is \",1;\" and I'm ,2; years old,22;",
+                (name) => {
+                    ran = true;
+                    expect(name).to.equal("Rowan");
+                }]
+        ]);
+        if (!ran) {
+            expect.fail("Did not run case code");
+        }
+    });
+
 });
